3d4466c8a38d3fd3bfbc67c7ecc33a79653504ce
dylancdavis <dylcdav@gmail.com>
2023-03-10 20:39:12 -0500

    Add basic scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,75 @@
+<script setup>
+import { EssencePanels } from './components/EssencePanels.vue'
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Welcome to Your Vue.js App</h1>
+  <div id="content">
+    <header>Alchemy Game</header>
+    <EssencePanels />
+  </div>
 </template>
+
+<style scoped> 
+
+* {
+  font-family: 'Open Sans';
+}
+
+#content {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: min-content 1fr
+}
+
+header {
+  background-color: rgb(75, 75, 75);
+  color: white;
+  padding: 16px;
+  padding-left: 64px;
+  font-size: 64px;
+  font-weight: bold;
+  text-shadow: 3px 3px 3px black;
+}
+
+#main-content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+#main-content > div {
+  background-color: rgb(58, 58, 58);
+  color: white;
+  height: 100%;
+}
+
+#left-panel {
+  font-size: 24px;
+  padding: 16px;
+  width: 30%;
+}
+
+#right-panel {
+  font-size: 24px;
+  flex: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px
+}
+
+#right-panel>div {
+  flex: 1;
+}
+
+.white-border {
+  border: 2px solid white;
+  padding: 8px;
+  border-radius: 16px;
+  background-color: rgb(48, 48, 48);
+}
+
+
+
+</style>
---
+++ src/components/EssencePanels.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+	<div id="essence-panels">
+		<div id="text-panel" class="panel">some text</div>
+		<div id="core-panel" class="panel">cores</div>
+		<div id="thought-panel" class="panel">thoughts</div>
+	</div>
+</template>
+
+<style scoped>
+
+	#essence-panels {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		grid-gap: 8px;
+		background-color: darkgray;
+	}
+
+	.panel {
+		padding: 16px;
+	}
+</style>
