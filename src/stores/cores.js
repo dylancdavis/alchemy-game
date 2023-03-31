4dcd274ad8a60586af60e742cfb16294fc8508df
dylancdavis <dylcdav@gmail.com>
2023-03-31 19:28:39 -0400

    Add stores for science and core data

---
+++ src/stores/cores.js
@@ -0,0 +1,25 @@
+// Cores store that keep track of essence and essenceMax
+
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+const useCoresStore = defineStore("cores", () => {
+  const essence = ref(0);
+  const essenceMax = ref(0);
+
+  function incrementEssenceBy(amt) {
+    essence.value += amt;
+    if (essence.value > essenceMax.value) {
+      essence.value = essenceMax.value;
+    }
+  }
+
+  function incrementEssenceMaxBy(amt) {
+    essenceMax.value += amt;
+  }
+
+  return { essence, essenceMax, incrementEssenceBy, incrementEssenceMaxBy };
+});
+
+export const trueCore = useCoresStore();
+export default trueCore;
---
+++ src/stores/science.js
@@ -0,0 +1,23 @@
+// Science store using Pinia setup api
+// store should keep track of variables named data and knowledge
+
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+const useScienceStore = defineStore("science", () => {
+  const data = ref(0);
+  const knowledge = ref(0);
+  function incrementData() {
+    data.value++;
+  }
+  function convertDataToKnowledge() {
+    if (data.value > 0) {
+      data.value--;
+      knowledge.value++;
+    }
+  }
+  return { data, knowledge, incrementData, convertDataToKnowledge };
+});
+
+const science = useScienceStore();
+export default science;
