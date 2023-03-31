fd24d17a038acc42d969cf3228927bed97c4f0f5
dylancdavis <dylcdav@gmail.com>
2023-03-31 11:40:40 -0400

    Trim unnecessary styles from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <script setup>
-import EssencePanels from './components/EssencePanels.vue'
+import EssencePanels from "./components/EssencePanels.vue";
 </script>
 
 <template>
   <v-app id="content">
-    <header>Alchemy Game</header>
+    <h1>Alchemy Game</h1>
     <EssencePanels />
   </v-app>
 </template>
 
-<style scoped> 
-
+<style scoped>
 * {
-  font-family: 'Open Sans';
+  font-family: "Open Sans";
 }
 
-#content {
-  display: grid;
-  grid-template-rows: min-content 1fr
-}
-
-header {
+h1 {
   background-color: rgb(75, 75, 75);
   color: white;
   padding: 16px;
@@ -29,45 +23,4 @@ header {
   font-weight: bold;
   text-shadow: 3px 3px 3px black;
 }
-
-#main-content {
-  display: flex;
-  width: 100%;
-  height: 100%;
-}
-
-#main-content > div {
-  background-color: rgb(58, 58, 58);
-  color: white;
-  height: 100%;
-}
-
-#left-panel {
-  font-size: 24px;
-  padding: 16px;
-  width: 30%;
-}
-
-#right-panel {
-  font-size: 24px;
-  flex: 1;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  gap: 16px
-}
-
-#right-panel>div {
-  flex: 1;
-}
-
-.white-border {
-  border: 2px solid white;
-  padding: 8px;
-  border-radius: 16px;
-  background-color: rgb(48, 48, 48);
-}
-
-
-
 </style>
