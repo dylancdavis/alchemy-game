31e1b67f475b5515cf3fd21bf9691a7a7d5e1171
dylancdavis <dylcdav@gmail.com>
2023-04-12 14:43:55 -0400

    Add gradient styling to core buttons

--- src/components/CoresPanel.vue
+++ src/components/CoresPanel.vue
@@ -64,19 +64,21 @@ const expandCore = () => {
           <div class="button-box">
             <v-btn
               :ripple="false"
-              class="gather"
+              class="gradient-button purple"
               @click="onGatherData"
               :disabled="disableGatherData"
               >Gather Data</v-btn
             >
             <v-btn
               :ripple="false"
+              class="gradient-button blue"
               @click="expandCore"
               v-show="science.manipulation"
               >Expand</v-btn
             >
             <v-btn
               :ripple="false"
+              class="gradient-button red"
               @click="contractCore"
               v-show="science.manipulation"
               >Contract</v-btn
@@ -109,15 +111,56 @@ const expandCore = () => {
   display: flex;
   gap: 1em;
   padding: 1em;
-  background-color: rgba(0, 0, 0, 0.355);
-  border: 2px solid black;
+}
+
+.gradient-button {
+  position: relative;
+  z-index: 1;
+}
+
+.gradient-button::before {
+  position: absolute;
+  content: "";
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  transition: opacity 0.5s linear;
+  opacity: 0;
+  border-radius: inherit;
+}
+
+.gradient-button.purple {
+  background-color: #f9dcff;
+}
+
+.gradient-button.purple::before {
+  background-image: linear-gradient(45deg, #f9dcff 0%, #ceb1fd 100%);
+}
+
+.gradient-button.blue {
+  background-color: #dbfaff;
+}
+
+.gradient-button.blue::before {
+  background-image: linear-gradient(45deg, #dbfaff 0%, #b1c0fc 100%);
+}
+
+.gradient-button.red {
+  background-color: #ffdbdb;
+}
+
+.gradient-button.red::before {
+  background-image: linear-gradient(45deg, #ffdbdb 0%, #fcb1b1 100%);
+}
+
+.gradient-button:hover::before {
+  opacity: 1;
 }
 
 .v-window-item {
   display: flex;
   flex-direction: column;
-}
-
-.selected {
 }
 </style>
